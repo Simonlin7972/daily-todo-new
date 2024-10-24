eefc4064c19acc9b5ecb468a91576f8c343ff3d1
Simonlin7972 <a97210230@gmail.com>
2024-10-24 14:20:11 +1100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': '/src',},
   },
   base: 'daily-todo-new',
 })
